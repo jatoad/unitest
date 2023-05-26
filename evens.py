eb9a7623d6783b1f7d615b0dda67e80ceaefbe3e
jatoad <jasperhmca.todd@gmail.com>
2023-05-26 11:13:49 +0000

    wrote and tested code to meet criteria

--- evens.py
+++ evens.py
@@ -6,8 +6,14 @@ def even_number_of_evens(numbers):
     if the number of even numbers is odd - return False
     if the numner of even numbers is even - return True
     """
+    if isinstance(numbers, list):
+        evens = sum([1 for n in numbers if n % 2 == 0])
+        
+        return True if evens and evens % 2 == 0 else False
 
+    else:
+        raise TypeError("A list was not passed into the function")
     return None
 
 if __name__ == "__main__":
-    print(even_number_of_evens(5))
+    print(even_number_of_evens([2, 1, 4]))
